ccc7434b8c3dc4faa946b20eeb08d70eed7cd17f
Chandra Sekhar <chandrasekhar4321n@gmail.com>
2026-03-02 10:08:05 +0530

    updated env for the depoly

--- server/src/index.ts
+++ server/src/index.ts
@@ -11,7 +11,7 @@ import  ChatRouter from "./routers/chats.js"
  const app = Express()
  app.use(Express.json())
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "https://chat-application-ps2v.onrender.com",
     credentials: true
 }));
 app.use(cookieParser())

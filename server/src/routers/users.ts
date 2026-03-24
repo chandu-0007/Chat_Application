71fb8c4c72cda4553efcbc23392cb5e9b667420e
Chandra Sekhar <chandrasekhar4321n@gmail.com>
2026-03-24 11:11:22 +0530

    add the cookies secure value true

--- server/src/routers/users.ts
+++ server/src/routers/users.ts
@@ -64,7 +64,7 @@ router.post("/register", async (req: Request, res: Response) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: true,
-      sameSite: "lax",
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
     });
 
@@ -131,7 +131,7 @@ router.post("/login", async (req: Request, res: Response) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: true,
-      sameSite: "lax",
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000
     });
 
